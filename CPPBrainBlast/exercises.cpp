173ca799bbf8a547a761ecd7debe803152a7a3ae
emoryhubbard <edhthe4th@gmail.com>
2023-12-22 23:53:43 -0500

    Renamed main file to exercises

--- CPPBrainBlast/exercises.cpp
+++ CPPBrainBlast/exercises.cpp
@@ -1,4 +1,4 @@
-// CPPBrainBlast.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// exercises.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
